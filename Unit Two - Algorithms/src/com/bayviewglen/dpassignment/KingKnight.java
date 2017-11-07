ec7e1b67644b4045d7f90de1274cf3e0322a6990
atapia1 <atapia1@atapia1sp3.bayviewglen.ca>
2017-11-06 21:17:27 -0500

    DO NOT MARK DID FOR LOLZ

--- Unit Two - Algorithms/src/com/bayviewglen/dpassignment/KingKnight.java
+++ Unit Two - Algorithms/src/com/bayviewglen/dpassignment/KingKnight.java
@@ -1,67 +1,81 @@
 package com.bayviewglen.dpassignment;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class KingKnight {
-	//START END POINT : What's the best solution
+	// START END POINT : What's the best solution
 	public static void main(String[] args) {
-		int n = 5;
-		//int knight = ;
-		int king = 1;
-		int[] start = {2,0};
-		int[] end = {0, 3};
-		int[][] moves = {{0,1},{1,1},{1,0},{1,2},{2,1}};
-		
+		long t = System.currentTimeMillis();
+		read();
+		System.out.println("Time: " + (System.currentTimeMillis()-t));
+	}
 
-		int[] positions = new int[5+5];
-		int j = 0;
-		while(start[0] !=end[0] && start[1] != end[1]) {
-			j++;
-			if(start[0] < end[0] && start[1] < end[1]) {
-				System.out.println("q1");
-			}else if(start[0] > end[0] && start[1] < end[1]) {
-				System.out.println("q2");
-				for(int i = 0; i<moves.length; i++) {
-					moves[i][0] = -moves[i][0];
-				}		
-			}else if(start[0] > end[0] && start[1] > end[1]) {
-				System.out.println("q3");
-				for(int i = 0; i<moves.length; i++) {
-					moves[i][0] = -moves[i][0];
-					moves[i][1] = -moves[i][1];
-				}
-			}else if(start[0] < end[0] && start[1] > end[1]) {
-				System.out.println("q4");
-				for(int i = 0; i<moves.length; i++) {
-					moves[i][1] = -moves[i][1];
-				}
+	public static void read() {
+		try {
+			Scanner in = new Scanner(new File("data/KingKnight.dat"));
+			int t = in.nextInt();
+			for (int i = 0; i < t; i++) {
+				int n = in.nextInt();
+				int[] start = new int[2];
+				int[] end = new int[2];
+				start[0] = in.nextInt();
+				start[1] = in.nextInt();
+				end[0] = in.nextInt();
+				end[1] = in.nextInt();
+
+				solve(n, start, end);
+
 			}
-			start = bestMove(start, moves, end);
-			System.out.println(start[0] + ", " + start[1]);
 
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
+	}
 
+	public static void solve(int n, int[] start, int[] end) {
+		// int n = 5;
+		// int knight = ;
+		int king = 1;
+		// int[] start = {2,0};
+		// int[] end = {0, 3};
+		// int[][] movesQ1 = {{0,1},{1,1},{1,0},{1,2},{2,1}};
+
+		int[][] moves = { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 },
+				{ 1, 2 }, { 2, 1 }, { 1, -2 }, { 2, -1 }, { -1, -2 }, { -2, -1 }, { -2, 1 }, { -1, 2 } };
+
+		int j = 0;
+		// System.out.println(start[0] + ", " + start[1]);
+		// System.out.println(end[0] + ", " + end[1]);
+
+		while (start[0] != end[0] || start[1] != end[1]) {
+			start = bestMove(n, start, moves, end);
+			j++;
 		}
 		System.out.println(j);
-		
+
 	}
-	public static int[] bestMove(int[] curr, int[][] move, int[] end) {
-		int minDist = Integer.MAX_VALUE;
-		int bestMove[] =  new int[2];
-		
-		for(int i = 0; i < move.length; i++) {
-			System.out.println("wdww");
+
+	public static int[] bestMove(int n, int[] curr, int[][] move, int[] end) {
+
+		double minDist = Integer.MAX_VALUE;
+		int bestMove[] = new int[2];
+
+		for (int i = 0; i < move.length; i++) {
 			int newX = curr[0] + move[i][0];
 			int newY = curr[1] + move[i][1];
-			int newDist = (int) Math.sqrt(Math.pow(newX-end[0], 2) + Math.pow(newY-end[1], 2));
-			
-			if(newDist < minDist) {
-				System.out.println("fe");
+			double newDist = Math.sqrt(Math.pow(newX - end[0], 2) + Math.pow(newY - end[1], 2));
+
+			if (newDist < minDist && newX <= n && newY <= n) {
 				bestMove[0] = newX;
 				bestMove[1] = newY;
 				minDist = newDist;
 			}
 
 		}
-		
 		return bestMove;
 	}
 
