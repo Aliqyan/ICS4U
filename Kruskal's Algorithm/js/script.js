501a26958360de4a692e44134901d0b21f6b11b0
Aliqyan <atapia1@bayviewglen.ca>
2017-12-17 23:14:50 -0500

    Better style and optimized canvas loading

--- Kruskal's Algorithm/js/script.js
+++ Kruskal's Algorithm/js/script.js
@@ -154,80 +154,48 @@ function showCommands(){
     for(x in commands){
         var curr = stack[step][x];
         //if(curr[0]){}
-            if(curr[0] == "t"){
+        if(curr != null){
+            if(curr[0] === "t"){
             drawText(curr[1], curr[2], curr[3], curr[4], curr[5]);
-            }else if(curr[0] == "tc"){
+            }else if(curr[0] === "tc"){
             drawTextConstrainted(curr[1], curr[2], curr[3], curr[4], curr[5], curr[6]);
-            }else if(curr[0] == "dc"){
+            }else if(curr[0] === "dc"){
             drawCircle(curr[1], curr[2], curr[3], curr[4]);
-            }else if(curr[0] == "dl"){
+            }else if(curr[0] === "dl"){
             drawLine(curr[1], curr[2], curr[3], curr[4]);
             }
+        }
         //}
     }
 }
 
-var commands = {
-    // connecting lines
-    "a-b": ["dl", v.a.x, v.a.y, v.b.x, v.b.y], // A-B
-    "a-c": ["dl", v.a.x, v.a.y, v.c.x, v.c.y], // A-C
-    "a-e": ["dl", v.a.x, v.a.y, v.e.x, v.e.y], // A-E
-    "a-f": ["dl", v.a.x, v.a.y, v.f.x, v.f.y], // A-F
-    "b-c": ["dl", v.b.x, v.b.y, v.c.x, v.c.y], // B-C
-    "b-d": ["dl", v.b.x, v.b.y, v.d.x, v.d.y], // B-D
-    "c-d": ["dl", v.c.x, v.c.y, v.d.x, v.d.y], // C-D
-    "c-e": ["dl", v.c.x, v.c.y, v.e.x, v.e.y], // C-E
-    "f-g": ["dl", v.f.x, v.f.y, v.g.x, v.g.y], // F-G
-    "e-f": ["dl", v.e.x, v.e.y, v.f.x, v.f.y], // E-F
-    "e-g": ["dl", v.e.x, v.e.y, v.g.x, v.g.y], // E-G
-    "d-g": ["dl", v.d.x, v.d.y, v.g.x, v.g.y], // D-G
 
-    "a-b val": ["t", "2", 30, (v.a.x + v.b.x) / 2 - 5, (v.a.y + v.b.y) / 2 - 10, "#d3d0cb"], // A-B
-    "a-c val": ["t", "11", 30, (v.a.x + v.c.x) / 2, (v.a.y + v.c.y) / 2 - 10, "#d3d0cb"], // A-C
-    "a-e val": ["t", "6", 30, (v.a.x + v.e.x) / 2, (v.a.y + v.e.y) / 2 + 45, "#d3d0cb"], // A-E
-    "a-f val": ["t", "4", 30, (v.a.x + v.f.x) / 2 - 25, (v.a.y + v.f.y) / 2 + 15, "#d3d0cb"], // A-F
-    "b-c val": ["t", "3", 30, (v.b.x + v.c.x) / 2 - 25, (v.b.y + v.c.y) / 2, "#d3d0cb"], // B-C
-    "b-d val": ["t", "9", 30, (v.b.x + v.d.x) / 2, (v.b.y + v.d.y) / 2 - 10, "#d3d0cb"], // B-D
-    "c-d val": ["t", "7", 30, (v.c.x + v.d.x) / 2, (v.c.y + v.d.y) / 2 - 5, "#d3d0cb"], // C-D
-    "c-e val": ["t", "2", 30, (v.c.x + v.e.x) / 2 + 15, (v.c.y + v.e.y) / 2 + 30, "#d3d0cb"], // C-E
-    "f-g val": ["t", "12", 30, (v.f.x + v.g.x) / 2, (v.f.y + v.g.y) / 2 + 35, "#d3d0cb"], // F-G
-    "e-f val": ["t", "7", 30, (v.e.x + v.f.x) / 2 + 25, (v.e.y + v.f.y) / 2 + 15, "#d3d0cb"], // E-F
-    "e-g val": ["t", "8", 30, (v.e.x + v.g.x) / 2, (v.e.y + v.g.y) / 2 - 10, "#d3d0cb"], // E-G
-    "d-g val": ["t", "5", 30, (v.d.x + v.g.x) / 2 + 20, (v.d.y + v.g.y) / 2 - 5, "#d3d0cb"], // D-G
-
-};
-
-// to del put the key and "del"
- var changes = [
-    [
-
-    ],
-    [
-        ["a-c", "del"],
-        ["a-b", ["dl", v.a.x, v.a.y, v.b.x, v.b.y]], // A-B
-
-
-
-    ],
-
-];
 
 //create a stack conatining eachs step to allow easy switching between steps
 var stack = [];
 stack.push(commands);
 for(var i = 1; i < changes.length; i++){
-    var curr = JSON.parse(JSON.stringify(stack[i-1]));
+    var curr = {};
+    for (let key in stack[i-1]) {
+        curr[key] = stack[i-1][key];
+    }
 
-    var newCommands = changes[i];
-    for(var i = 0; i < newCommands.length; i++){
-        if(newCommands[1] === "del"){
-                delete curr[0];
-        }
-        curr[newCommands[i][0]] = newCommands[i][1];
+    //var curr = JSON.parse(JSON.stringify(stack[i-1]));
+
+
+    for(var j = 0; j < changes[i].length; j++){
+        var newCommand = changes[i][j];
+        /*if(newCommands[i][1] === "del"){
+            delete curr[0];
+        }else{*/
+                    //onsole.log(curr["hi"] = newCommand[1]);
+            //Object.defineProperty(curr, newCommand[0],  newCommand[1]); 
+            curr[newCommand[0]] = newCommand[1];
+        //}
     }
     stack.push(curr)
 }
-
+//console.log(stack.length);
 
 /*
 var graph = {
@@ -286,18 +254,7 @@ var graph = {
     ["t", "F", 30, v.f.x, v.f.y + 10, "#d3d0cb"],
     ["t", "G", 30, v.g.x, v.g.y + 10, "#d3d0cb"],
 
-    ["t", "2", 30, (v.a.x + v.b.x) / 2 - 5, (v.a.y + v.b.y) / 2 - 10, "#d3d0cb"], // A-B
-    ["t", "11", 30, (v.a.x + v.c.x) / 2, (v.a.y + v.c.y) / 2 - 10, "#d3d0cb"], // A-C
-    ["t", "6", 30, (v.a.x + v.e.x) / 2, (v.a.y + v.e.y) / 2 + 45, "#d3d0cb"], // A-E
-    ["t", "4", 30, (v.a.x + v.f.x) / 2 - 25, (v.a.y + v.f.y) / 2 + 15, "#d3d0cb"], // A-F
-    ["t", "3", 30, (v.b.x + v.c.x) / 2 - 25, (v.b.y + v.c.y) / 2, "#d3d0cb"], // B-C
-    ["t", "9", 30, (v.b.x + v.d.x) / 2, (v.b.y + v.d.y) / 2 - 10, "#d3d0cb"], // B-D
-    ["t", "7", 30, (v.c.x + v.d.x) / 2, (v.c.y + v.d.y) / 2 - 5, "#d3d0cb"], // C-D
-    ["t", "2", 30, (v.c.x + v.e.x) / 2 + 15, (v.c.y + v.e.y) / 2 + 30, "#d3d0cb"], // C-E
-    ["t", "12", 30, (v.f.x + v.g.x) / 2, (v.f.y + v.g.y) / 2 + 35, "#d3d0cb"], // F-G
-    ["t", "7", 30, (v.e.x + v.f.x) / 2 + 25, (v.e.y + v.f.y) / 2 + 15, "#d3d0cb"], // E-F
-    ["t", "8", 30, (v.e.x + v.g.x) / 2, (v.e.y + v.g.y) / 2 - 10, "#d3d0cb"], // E-G
-    ["t", "5", 30, (v.d.x + v.g.x) / 2 + 20, (v.d.y + v.g.y) / 2 - 5, "#d3d0cb"], // D-G
+
 };
 */
 
