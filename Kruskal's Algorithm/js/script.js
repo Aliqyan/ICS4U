825a7053edb98ba29a15a998cbbbdd8aaf59d1c0
iMac-Aliqyan <aliqyantapia@gmail.com>
2018-01-07 21:49:10 -0500

    Push

--- Kruskal's Algorithm/js/script.js
+++ Kruskal's Algorithm/js/script.js
@@ -5,12 +5,10 @@ var step = 0;//changes.length-4;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-canvas.addEventListener("mousedown", getPosition, false);
+//canvas.addEventListener("mousedown", getPosition, false);
 
-var rightPressed = false;
-var leftPressed = false;
-var s = new Date();
-var start = s.getTime();
+
+
 
 var s2 = new Date();
 var start2 = s2.getTime();
@@ -26,11 +24,9 @@ function keyDownHandler(e) {
         }
 
     } else*/ if (e.keyCode == 32) {
-        leftPressed = true;
         if(step < changes.length-1){
             step++;
-            s = new Date();
-            start = s.getTime();
+
             s2 = new Date();
             start2 = s2.getTime();
             //updateCommands(step);
@@ -87,7 +83,7 @@ function drawText(text, size, posX, posY, color) {
 }
 
 
-function drawTextConstrainted(text, size, posX, posY, color, limitX) {
+/*function drawTextConstrainted(text, size, posX, posY, color, limitX) {
     ctx.font = size + "px Arial";
     ctx.fillStyle = color;
     ctx.textAlign = "left";
@@ -111,16 +107,16 @@ function drawTextConstrainted(text, size, posX, posY, color, limitX) {
 
     }
 
-}
+}*/
 
-function drawArrows() {
+/*function drawArrows() {
     var prev = new Image(100, 100);
     prev.src = 'images/prev.png';
     ctx.drawImage(prev, 0, canvas.height / 2, 25, 25);
     var next = new Image(100, 100);
     next.src = 'images/next.png';
     ctx.drawImage(next, canvas.width - 25, canvas.height / 2, 25, 25);
-}
+}*/
 
 function drawCircle(posX, posY , radius, color){
     ctx.beginPath();
@@ -142,30 +138,22 @@ function drawLine(posX1, posY1, posX2, posY2, color, size){
     ctx.stroke();
 }
 
-var n = new Date();
-var now = n.getTime();
+
 
 function showCommands(){
     for(x in commands){
         var curr = stack[step][x];
-        n = new Date();
-        now = n.getTime();
+
         if(curr != null){
-            if(!curr[1]){
-                if(curr[0] >= 0 && now-start >= curr[0]){
-                    curr[1] = true; 
-                }   
-            }else{
-                if(curr[2] === "t"){
-                drawText(curr[3], curr[4], curr[5], curr[6], curr[7]);
-                }else if(curr[2] === "tc"){
-                drawTextConstrainted(curr[3], curr[4], curr[5], curr[6], curr[7], curr[8]);
-                }else if(curr[2] === "dc"){
-                drawCircle(curr[3], curr[4], curr[5], curr[6]);
-                }else if(curr[2] === "dl"){
-                drawLine(curr[3], curr[4], curr[5], curr[6], curr[7]);
+            
+                if(curr[0] === "t"){
+                drawText(curr[1], curr[2], curr[3], curr[4], curr[5]);
+                }else if(curr[0] === "dc"){
+                drawCircle(curr[1], curr[2], curr[3], curr[4]);
+                }else if(curr[0] === "dl"){
+                drawLine(curr[1], curr[2], curr[3], curr[4], curr[5]);
                 }
-            }
+            
         }
     }
 }
@@ -213,7 +201,7 @@ function nextCommand(){
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawArrows();
+    //drawArrows();
     showCommands();
     nextCommand();
 
